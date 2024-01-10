ad7f9345764ed4159e1f905d86ef5f096d3d1065
ZhangZh331177 <147566896+ZhangZh331177@users.noreply.github.com>
2024-01-10 18:38:24 +0800

    Upload files of user.

---
+++ FinalRelease/CRS_code/user/cbvs.py
@@ -0,0 +1,169 @@
+from django.shortcuts import reverse, redirect
+from django.views.generic import CreateView, UpdateView
+
+from user.forms import StuRegisterForm, TeaRegisterForm, ManRegisterForm
+
+from user.models import Student, Teacher, Manager
+# import random
+ 
+class CreateStudentView(CreateView):
+    model = Student
+    form_class = StuRegisterForm
+    # fields = "__all__"
+    template_name = "user/register.html"
+    success_url = "login"
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            self.object = None
+            return self.form_invalid(form)
+    def form_valid(self, form):
+        # 【已废弃规则】学生注册时选定年级自动生成学号
+        # grade = form.cleaned_data["grade"]
+        # order_by默认升序排列，number前的负号表示降序排列
+        # student_set = Student.objects.filter(grade=grade).order_by("-number")
+        # if student_set.count() > 0:
+        #     last_student = student_set[0]
+        #     new_number = str(int(last_student.number) + 1)
+        #     for i in range(6 - len(new_number)):
+        #         new_number = "0" + new_number
+        # else:
+        #     new_number = "000001"
+
+        # Create, but don't save the new student instance.
+        new_student = form.save(commit=False)
+        # Modify the student
+        # new_student.number = new_number
+        # Save the new instance.
+        new_student.save()
+        # Now, save the many-to-many data for the form.
+        form.save_m2m()
+
+        self.object = new_student
+
+        uid = new_student.number
+        from_url = "register"
+        base_url = reverse(self.get_success_url(), kwargs={'kind': 'student'})
+        return redirect(base_url + '?uid=%s&from_url=%s' % (uid, from_url))
+
+    def get_context_data(self, **kwargs):
+        context = super(CreateStudentView, self).get_context_data(**kwargs)
+        context["kind"] = "student"
+
+        return context
+
+class CreateTeacherView(CreateView):
+    model = Teacher
+    form_class = TeaRegisterForm
+    template_name = "user/register.html"
+    success_url = "login"
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            self.object = None
+            return self.form_invalid(form)
+    def form_valid(self, form):
+        # 【已废弃规则】老师注册时随机生成院系号, 院系号范围为[0,300)
+        # department_no = random.randint(0, 300)
+        # 把非三位数的院系号转换为以0填充的三位字符串，如1转换为'001'
+        # department_no = '{:0>3}'.format(department_no)
+        # teacher_set = Teacher.objects.filter(department_no=department_no).order_by("-number")
+        # if teacher_set.count() > 0:
+        #     last_teacher = teacher_set[0]
+        #     new_number = int(last_teacher.number) + 1
+        #     new_number = '{:0>7}'.format(new_number)
+        # else:
+        #     new_number = "0000001"
+
+        # Create, but don't save the new teacher instance.
+        new_teacher = form.save(commit=False)
+        # Modify the teacher
+        # new_teacher.department_no = department_no
+        # new_teacher.number = new_number
+        # Save the new instance.
+        new_teacher.save()
+        # Now, save the many-to-many data for the form.
+        form.save_m2m()
+
+        self.object = new_teacher
+
+        uid = new_teacher.number
+        from_url = "register"
+        base_url = reverse(self.get_success_url(), kwargs={'kind': 'teacher'})
+        return redirect(base_url + '?uid=%s&from_url=%s' % (uid, from_url))
+    
+    def get_context_data(self, **kwargs):
+        context = super(CreateTeacherView, self).get_context_data(**kwargs)
+        context["kind"] = "teacher"
+
+        return context
+
+class CreateManagerView(CreateView):
+    model = Manager
+    form_class = ManRegisterForm
+    template_name = "user/register.html"
+    success_url = "login"
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            self.object = None
+            return self.form_invalid(form)
+    def form_valid(self, form):
+        # Create, but don't save the new teacher instance.
+        new_manager = form.save(commit=False)
+        # Save the new instance.
+        new_manager.save()
+        # Now, save the many-to-many data for the form.
+        form.save_m2m()
+        self.object = new_manager
+        uid = new_manager.number
+        from_url = "register"
+        base_url = reverse(self.get_success_url(), kwargs={'kind': 'manager'})
+        return redirect(base_url + '?uid=%s&from_url=%s' % (uid, from_url))
+    def get_context_data(self, **kwargs):
+        context = super(CreateManagerView, self).get_context_data(**kwargs)
+        context["kind"] = "manager"
+        return context
+    
+
+class UpdateStudentView(UpdateView):
+    model = Student
+    form_class = StuRegisterForm
+    template_name = "user/update.html"
+    def get_context_data(self, **kwargs):
+        context = super(UpdateStudentView, self).get_context_data(**kwargs)
+        context.update(kwargs)
+        context["kind"] = "student"
+        return context
+    def get_success_url(self):
+        return reverse("course", kwargs={"kind": "student"})
+
+class UpdateTeacherView(UpdateView):
+    model = Teacher
+    form_class = TeaRegisterForm
+    template_name = "user/update.html"
+    def get_context_data(self, **kwargs):
+        context = super(UpdateTeacherView, self).get_context_data(**kwargs)
+        context.update(kwargs)
+        context["kind"] = "teacher"
+        return context
+    def get_success_url(self):
+        return reverse("course", kwargs={"kind": "teacher"})
+
+class UpdateManagerView(UpdateView):
+    model = Manager
+    form_class = ManRegisterForm
+    template_name = "user/update.html"
+    def get_context_data(self, **kwargs):
+        context = super(UpdateManagerView, self).get_context_data(**kwargs)
+        context.update(kwargs)
+        context["kind"] = "manager"
+        return context
+    def get_success_url(self):
+        return reverse("course", kwargs={"kind": "manager"})
---
+++ FinalRelease/CRS_code/user/models.py
@@ -0,0 +1,101 @@
+# usr/bin/env python3
+# -*- coding:utf-8- -*-
+from django.db import models
+from django.core.exceptions import ValidationError
+
+# 定义验证器
+def validate_pas_length(value):
+    if len(value) < 6 or len(value) > 12:
+        raise ValidationError('密码的长度应为6~12个字符')
+def validate_stu_length(value):
+    if (value<1e11) or (value>=1e12):
+        raise ValidationError('学号应为非0开头的12位数字')
+def validate_length(value):
+    if (value<1e4)or (value>=1e5):
+        raise ValidationError('教职工号的长度应为非0开头的5位数字')
+    
+class Student(models.Model):
+    genders = [
+        ("m", "男"),
+        ("f", "女")
+    ]
+    grades = [
+        (1, 2017),
+        (2, 2018),
+        (3, 2019),
+        (4, 2020),
+        (5, 2021),
+        (6, 2022),
+        (7, 2023),
+        (8, 2024)
+    ]
+
+    name = models.CharField(max_length=50, verbose_name="姓名")
+    gender = models.CharField(max_length=10, choices=genders, default='m', verbose_name="性别")
+    # birthday = models.DateField(verbose_name="生日")
+    grade = models.IntegerField(choices=grades, default=1, verbose_name="入学年份")
+    number = models.BigIntegerField(verbose_name="学号", primary_key=True, validators=[validate_stu_length])
+    email = models.EmailField(max_length=254, verbose_name="邮箱")
+    password = models.CharField(max_length=30, verbose_name="密码", validators=[validate_pas_length])
+    # info = models.CharField(max_length=255, verbose_name="个人简介", null=True, default='无', help_text="自我介绍，不超过250字")
+    """
+    class Meta:
+        constraints = [
+            # 复合主键：保证 grade和number组合的student_id唯一
+            models.UniqueConstraint(fields=['grade', 'number'], name='student_id'),
+        ]
+    """
+    def get_id(self):
+        return self.number
+    
+    def __str__(self):
+        return "%s (%s)" % (self.number, self.name)
+
+
+class Teacher(models.Model):
+    genders = [
+        ("m", "男"),
+        ("f", "女")
+    ]
+    """
+    name = models.CharField(max_length=50, verbose_name="姓名")
+    gender = models.CharField(max_length=10, choices=genders, default='m', verbose_name="性别")
+    birthday = models.DateField(verbose_name="生日")
+    email = models.EmailField(verbose_name="邮箱")
+    info = models.CharField(max_length=255, verbose_name="教师简介", help_text="不要超过250字")
+
+    department_no = models.CharField(max_length=3, verbose_name="院系号")
+    number = models.CharField(max_length=7, verbose_name="院内编号")
+    password = models.CharField(max_length=30, verbose_name="密码")
+
+    class Meta:
+        constraints = [
+            # 复合主键：保证 department_no 和number组合的 teacher_id 唯一
+            models.UniqueConstraint(fields=['department_no', 'number'], name='teacher_id'),
+        ]
+    """
+    name = models.CharField(max_length=50, verbose_name="姓名")
+    gender = models.CharField(max_length=10, choices=genders, default='m', verbose_name="性别")
+    number = models.BigIntegerField(verbose_name="教职工号", primary_key=True, validators=[validate_length])
+    email = models.EmailField(max_length=254,verbose_name="邮箱")
+    password = models.CharField(max_length=30, verbose_name="密码", validators=[validate_pas_length])
+    # info = models.CharField(max_length=255, verbose_name="个人简介", null=True, default='无', help_text="自我介绍，不超过250字")
+
+    def get_teacher_list():
+        # 获取dynamic_field的所有可能值
+        return [(item.number, f"{item.name} ({item.number})") for item in Teacher.objects.all()]
+
+
+class Manager(models.Model):
+    genders = [
+        ("m", "男"),
+        ("f", "女")
+    ]
+    name = models.CharField(max_length=50, verbose_name="姓名")
+    gender = models.CharField(max_length=10, choices=genders, default='m', verbose_name="性别")
+    number = models.BigIntegerField(verbose_name="教职工号", primary_key=True, validators=[validate_length])
+    email = models.EmailField(max_length=254,verbose_name="邮箱")
+    password = models.CharField(max_length=30, verbose_name="密码", validators=[validate_pas_length])
+    # info = models.CharField(max_length=255, verbose_name="个人简介", null=True, default='无', help_text="自我介绍，不超过250字")
+
+
